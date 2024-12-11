7393b320b8079a09e84d9e1003ea9049708b08c3
kalior123 <97810980+kalior123@users.noreply.github.com>
2024-12-11 03:41:40 +0000

    1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template, request
+import textblob
 
 app = Flask(__name__)
 
@@ -7,6 +8,15 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/SA",methods=["GET","POST"])
+def SA():
+    return(render_template("SA.html"))
+
+@app.route("/SA_result",methods=["GET","POST"])
+def SA_result():
+    q = request.form.get("q")
+    r = textblob.TextBlob(q).sentiment
+    return(render_template("SA_result.html", r=r))
 
 if __name__ == "__main__":
     app.run()
